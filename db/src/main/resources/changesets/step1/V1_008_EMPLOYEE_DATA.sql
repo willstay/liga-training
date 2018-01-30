d661aca85bec2b622872fff6cb39ebfbb03e094d
sanasov <sanasov@phoenixit.ru>
2018-01-30 16:54:55 +0300

    db step1

--- db/src/main/resources/changesets/step1/V1_008_EMPLOYEE_DATA.sql
+++ db/src/main/resources/changesets/step1/V1_008_EMPLOYEE_DATA.sql
@@ -2,40 +2,40 @@
 --changeset sanasov:1.8 runOnChange:true context:prod
 
 INSERT INTO liga.employee ( id,  fio,  gender,  faculty,  degree,  employee_position,  birthday)
-VALUES(1, "Владимир Николаевич Чубариков", "male", 1, "Ph.D","Dean", "1951-09-05");
+VALUES(1, 'Владимир Николаевич Чубариков', 'male', 1, 'Ph.D','Dean', '1951-09-05');
 
 INSERT INTO liga.employee ( id,  fio,  gender,  faculty,  degree,  employee_position,  birthday)
-VALUES(2, "Сысоев Николай Николаевич", "male", 2, "Ph.D","Dean", "1949-11-15");
+VALUES(2, 'Сысоев Николай Николаевич', 'male', 2, 'Ph.D','Dean', '1949-11-15');
 
 INSERT INTO liga.employee ( id,  fio,  gender,  faculty,  degree,  employee_position,  birthday)
-VALUES(3, "Тучков Иван Иванович", "male", 3, "Ph.D","Dean", "1956-09-13");
-
-
-INSERT INTO liga.employee ( id,  fio,  gender,  faculty,  degree,  employee_position,  birthday)
-VALUES(4, "Комбаров Анатолий Петрович", "male", 1, "Ph.D","Lecturer", "1967-03-05");
-
-INSERT INTO liga.employee ( id,  fio,  gender,  faculty,  degree,  employee_position,  birthday)
-VALUES(5, "Корнев Андрей Алексеевич", "male", 1, "Ph.D","Lecturer", "1971-08-13");
-
-INSERT INTO liga.employee ( id,  fio,  gender,  faculty,  degree,  employee_position,  birthday)
-VALUES(6, "Штерн Александр Исаакович", "male", 1, "Ph.D","Lecturer", "1948-06-05");
+VALUES(3, 'Тучков Иван Иванович', 'male', 3, 'Ph.D','Dean', '1956-09-13');
 
 
 INSERT INTO liga.employee ( id,  fio,  gender,  faculty,  degree,  employee_position,  birthday)
-VALUES(7, "Караваев Владимир Александрович", "male", 2, "PhD","Lecturer", "1983-06-15");
+VALUES(4, 'Комбаров Анатолий Петрович', 'male', 1, 'Ph.D','Lecturer', '1967-03-05');
 
 INSERT INTO liga.employee ( id,  fio,  gender,  faculty,  degree,  employee_position,  birthday)
-VALUES(8, "Тверской Василий Борисович ", "male", 2, "Ph.D","Lecturer", "1967-02-28");
+VALUES(5, 'Корнев Андрей Алексеевич', 'male', 1, 'Ph.D','Lecturer', '1971-08-13');
 
 INSERT INTO liga.employee ( id,  fio,  gender,  faculty,  degree,  employee_position,  birthday)
-VALUES(9, "Соколов Владимир Андреевич", "male", 2, "Ph.D","Lecturer", "1975-08-07");
+VALUES(6, 'Штерн Александр Исаакович', 'male', 1, 'Ph.D','Lecturer', '1948-06-05');
 
 
 INSERT INTO liga.employee ( id,  fio,  gender,  faculty,  degree,  employee_position,  birthday)
-VALUES(10, "Гутнова, Евгения Владимировна", "female", 3, "PhD","Lecturer", "1969-10-23");
+VALUES(7, 'Караваев Владимир Александрович', 'male', 2, 'PhD','Lecturer', '1983-06-15');
 
 INSERT INTO liga.employee ( id,  fio,  gender,  faculty,  degree,  employee_position,  birthday)
-VALUES(11, "Дыбо, Анна Владимировна", "female", 3, "PhD","Lecturer", "1978-07-10");
+VALUES(8, 'Тверской Василий Борисович ', 'male', 2, 'Ph.D','Lecturer', '1967-02-28');
+
+INSERT INTO liga.employee ( id,  fio,  gender,  faculty,  degree,  employee_position,  birthday)
+VALUES(9, 'Соколов Владимир Андреевич', 'male', 2, 'Ph.D','Lecturer', '1975-08-07');
+
+
+INSERT INTO liga.employee ( id,  fio,  gender,  faculty,  degree,  employee_position,  birthday)
+VALUES(10, 'Гутнова, Евгения Владимировна', 'female', 3, 'PhD','Lecturer', '1969-10-23');
+
+INSERT INTO liga.employee ( id,  fio,  gender,  faculty,  degree,  employee_position,  birthday)
+VALUES(11, 'Дыбо, Анна Владимировна', 'female', 3, 'PhD','Lecturer', '1978-07-10');
 
 
 
--- db/src/main/resources/changesets/step1/V1_010_STUDENT_GROUP_DATA.sql
+++ db/src/main/resources/changesets/step1/V1_010_STUDENT_GROUP_DATA.sql
@@ -1,20 +1,20 @@
 --liquibase formatted sql logicalFilePath:V1_010_STUDENT_GROUP_DATA.sql
 --changeset sanasov:1.10 runOnChange:true context:prod
 
-INSERT INTO liga.student_group (id,  group_number,  department) VALUES (1, 11, "Механико-математический");
-INSERT INTO liga.student_group (id,  group_number,  department) VALUES (2, 21, "Механико-математический");
-INSERT INTO liga.student_group (id,  group_number,  department) VALUES (3, 31, "Механико-математический");
-INSERT INTO liga.student_group (id,  group_number,  department) VALUES (4, 41, "Механико-математический");
+INSERT INTO liga.student_group (id,  group_number,  department) VALUES (1, 11, 'Механико-математический');
+INSERT INTO liga.student_group (id,  group_number,  department) VALUES (2, 21, 'Механико-математический');
+INSERT INTO liga.student_group (id,  group_number,  department) VALUES (3, 31, 'Механико-математический');
+INSERT INTO liga.student_group (id,  group_number,  department) VALUES (4, 41, 'Механико-математический');
 
-INSERT INTO liga.student_group (id,  group_number,  department) VALUES (6, 11, "Физический");
-INSERT INTO liga.student_group (id,  group_number,  department) VALUES (7, 21, "Физический");
-INSERT INTO liga.student_group (id,  group_number,  department) VALUES (8, 31, "Физический");
-INSERT INTO liga.student_group (id,  group_number,  department) VALUES (9, 41, "Физический");
+INSERT INTO liga.student_group (id,  group_number,  department) VALUES (6, 11, 'Физический');
+INSERT INTO liga.student_group (id,  group_number,  department) VALUES (7, 21, 'Физический');
+INSERT INTO liga.student_group (id,  group_number,  department) VALUES (8, 31, 'Физический');
+INSERT INTO liga.student_group (id,  group_number,  department) VALUES (9, 41, 'Физический');
 
-INSERT INTO liga.student_group (id,  group_number,  department) VALUES (11, 11, "Исторический");
-INSERT INTO liga.student_group (id,  group_number,  department) VALUES (12, 21, "Исторический");
-INSERT INTO liga.student_group (id,  group_number,  department) VALUES (13, 31, "Исторический");
-INSERT INTO liga.student_group (id,  group_number,  department) VALUES (14, 41, "Исторический");
+INSERT INTO liga.student_group (id,  group_number,  department) VALUES (11, 11, 'Исторический');
+INSERT INTO liga.student_group (id,  group_number,  department) VALUES (12, 21, 'Исторический');
+INSERT INTO liga.student_group (id,  group_number,  department) VALUES (13, 31, 'Исторический');
+INSERT INTO liga.student_group (id,  group_number,  department) VALUES (14, 41, 'Исторический');
 
 
 
