bce4c08c274763442b817cd292cd749ea7efc7b3
sanasov <sanasov@phoenixit.ru>
2018-02-02 12:44:47 +0300

    application.properties + maven profile

--- liga-orm/src/main/java/ru/liga/config/TestDaoSpringConfig.java
+++ liga-orm/src/main/java/ru/liga/config/TestDaoSpringConfig.java
@@ -1,6 +1,7 @@
 package ru.liga.config;
 
 import org.postgresql.ds.PGSimpleDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -8,11 +9,18 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import ru.liga.dao.DepartmentDao;
 
 
-
 @Configuration
 @PropertySource("classpath:application.properties")
 public class TestDaoSpringConfig {
 
+    @Value("${database.url}")
+    private String url;
+    @Value("${database.user}")
+    private String user;
+    @Value("${database.password}")
+    private String password;
+    @Value("${database.schema}")
+    private String schema;
 
     @Bean
     public DepartmentDao departmentDao() {
@@ -24,9 +32,9 @@ public class TestDaoSpringConfig {
         String url = "jdbc:postgresql://localhost:5432/postgres";
         PGSimpleDataSource dataSource = new PGSimpleDataSource();
         dataSource.setUrl(url);
-        dataSource.setUser("postgres");
-        dataSource.setPassword("q");
-        dataSource.setCurrentSchema("liga");
+        dataSource.setUser(user);
+        dataSource.setPassword(password);
+        dataSource.setCurrentSchema(schema);
         return new JdbcTemplate(dataSource);
     }
 }
