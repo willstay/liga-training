8eb14d917715304587f3fa4a8a487e3ca8eae602
sanasov <sanasov@phoenixit.ru>
2018-04-03 15:17:30 +0300

    liga orm hibernate spring boot

--- hibernate/src/test/java/ru/liga/hibernate/dao/DepartmentDaoTest.java
+++ hibernate/src/test/java/ru/liga/hibernate/dao/DepartmentDaoTest.java
@@ -1,32 +1,16 @@
-//package ru.liga.hibernate.dao;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import ru.liga.hibernate.config.TestDaoSpringConfig;
-//import ru.liga.hibernate.dao.DepartmentDao;
-//
-//
-//import static org.junit.Assert.assertEquals;
-//
-//
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(classes = {TestDaoSpringConfig.class})
-//public class DepartmentDaoTest {
-//
-//    @Autowired
-//    private DepartmentDao departmentDao;
-//
-//    @Before
-//    public void init() {
-//
-//    }
-//
-//    @Test
-//    public void select() {
-//        assertEquals(1, departmentDao.selectByFoundationYear(1933).size());
-//    }
-//
-//}
+package ru.liga.hibernate.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DepartmentDaoTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
