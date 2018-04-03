1f236ce020264f9cc7d03ab1a64039a7b64b7757
sanasov <sanasov@phoenixit.ru>
2018-04-03 15:36:23 +0300

    liga orm remove sessionFactory

--- hibernate/src/test/java/ru/liga/hibernate/dao/DepartmentDaoTest.java
+++ hibernate/src/test/java/ru/liga/hibernate/dao/DepartmentDaoTest.java
@@ -2,15 +2,25 @@ package ru.liga.hibernate.dao;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import ru.liga.hibernate.entity.DepartmentEntity;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DepartmentDaoTest {
+    @Autowired
+    private DepartmentDao departmentDao;
 
     @Test
-    public void contextLoads() {
+    public void findByIdTest() {
+        DepartmentEntity entity = departmentDao.select(1L);
+        assertEquals("Механико-математический", entity.getTitle());
+        assertEquals("119991, ГСП-1, Москва, Ленинские горы, МГУ, д.1, Главное здание, механико-математический факультет", entity.getAddress());
+        assertEquals(Integer.valueOf(1932), entity.getFoundationYear());
     }
 
 }
